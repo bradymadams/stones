90228dbdd7e4d43bb706fa4eee196edbc846bba0
brady <brady.m.adams@gmail.com>
2020-10-24 16:03:12 -0600

    rm matplotlib

--- stones.py
+++ stones.py
@@ -5,10 +5,6 @@ import weightdb
 
 from flask import Flask, render_template, request, jsonify, make_response
 
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
-from matplotlib.dates import DateFormatter
-
 app = Flask(__name__)
 app.debug = True
 
@@ -40,40 +36,6 @@ def weight_get(days=None):
     db = weightdb.WeightDb(DBNAME)
     wh = weightdb.WeightHistory(db, days)
     return jsonify(wh.dict_all())
-
-@app.route('/weight/plot/all')
-@app.route('/weight/plot/<int:days>')
-def weight_plot(days=None):
-    db = weightdb.WeightDb(DBNAME)
-    hist = db.get_weights(days)
-
-    d = []
-    w = []
-    for h in hist:
-        dstr = h[0]
-        if '.' in dstr:
-            idot = dstr.index('.')
-            dstr = dstr[:idot]
-        d.append(datetime.datetime.strptime(dstr, '%Y-%m-%d %H:%M:%S'))
-        w.append(h[1])
-
-    #fig = Figure(figsize=(2,1.5))
-    fig = Figure()
-    ax = fig.add_subplot(111)
-
-    ax.plot_date(d, w, '-')
-    ax.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
-
-    fig.autofmt_xdate()
-
-    canvas = FigureCanvas(fig)
-    png_output = io.StringIO()
-    canvas.print_png(png_output)
-
-    response = make_response(png_output.getvalue())
-    response.headers['Content-Type'] = 'image/png'
-
-    return response
 
 if __name__ == '__main__':
     app.run()
